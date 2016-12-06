dfb1fbb1621c990f082c620b9ffd66076766e17a
Caitlin Stein <caitlin.teter@gmail.com>
2016-12-06 11:01:03 -0600

    looping through info to populate dom

--- xhr.js
+++ xhr.js
@@ -3,15 +3,20 @@ console.log("hello from the xhr practice");
 function loadMessages(e){
   var data = JSON.parse(e.target.responseText)
   console.log("data", data)
-  //"Caitlin"
-  console.log(data.messages[0].sender)
-  //{"content": "Save Ferris!"... show the whole obj}
-  console.log("content: " + data.messages[2].content)
-  // "Dec 5th"
-  console.log(data.messages[1].date)
-  // BONUS: "Ferris"
-  console.log(data.messages[2].content.split(" ").pop())
-  console.log(data.messages[2].content.slice(4, 11));
+//for loop to fill dom 
+  var slackContent = ""
+  for (var i = 0; i < data.messages.length; i++) {
+    slackContent += ` <div>
+                        <date>${data.messages[i].date}</date>
+                        <h3>${data.messages[i].sender}</h3>
+                        <p>${data.messages[i].content}</p>
+                      </div>`
+
+  }
+
+
+//assign that stuff to #slack
+  document.getElementById("slack").innerHTML = slackContent
 
   
 }
